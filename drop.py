8605fd9eef699c555610453d9ad81d1d4ccfd08b
Ziqi <102622917+wazaqa-wah@users.noreply.github.com>
2022-05-05 11:50:32 -0400

    Update drop.py

--- drop.py
+++ drop.py
@@ -2,6 +2,7 @@ import csv
 import pandas as pd
 
 #drop all the special edition credits
+# this means that any string in Note that contians 1997 or 2004 for the first 3 movies need to be dropped.
 
 df = pd.read_csv('VI1983all.csv')
 
