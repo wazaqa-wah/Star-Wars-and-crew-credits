f635aa5b1dff73a1a52fc264c6e311bbf1bb1120
Ziqi <102622917+wazaqa-wah@users.noreply.github.com>
2022-05-05 11:50:56 -0400

    Update difference.py

--- difference.py
+++ difference.py
@@ -7,10 +7,10 @@ df_VI = pd.read_csv('VI1983all.csv')
 df_I = pd.read_csv('I1999all.csv')
 
 diff = ~df_I['Credit'].isin(df_VI['Credit'])
-#later.earlier
+#later film first .earlier film later
 
 #print(diff)
 
 VI_I_diff = (df_I[diff == True])
-#later
+#later film difference
 VI_I_diff.to_csv('diff_VI_I.csv', encoding='utf-8', index=False)
