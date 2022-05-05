cf2290a2336d4582141c9e847c90838ef0e0ed62
Ziqi <102622917+wazaqa-wah@users.noreply.github.com>
2022-05-05 11:51:17 -0400

    Update pull_out.py

--- pull_out.py
+++ pull_out.py
@@ -2,6 +2,7 @@ import csv
 import pandas as pd
 
 # this is for pulling out all the credits for special edition for the first trilogy
+# this means that all string in Note that continas 1997 or 2004 from the first 3 movies need to be pulled out
 
 df = pd.read_csv('VI1983all.csv')
 
